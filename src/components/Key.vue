4b91d7a045482cc180e42a59fa7bf6ed9b300d1e
mdmrk <mariodavo.20@gmail.com>
2024-03-14 13:00:34 +0100

    style: arrow buttons

--- src/components/Key.vue
+++ src/components/Key.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-gray-darker w-5 h-5 rounded-sm shadow-[0em_-0.15rem_0em_rgba(0,0,0,0.25)_inset] flex items-center justify-center [&>svg]:w-3 [&>svg]:h-3 stroke-gray-dark hover:translate-y-0.5"
+    class="bg-gray-darker w-5 h-5 rounded-sm shadow-[0em_-0.15rem_0em_rgba(0,0,0,0.25)_inset] flex items-center justify-center [&>svg]:w-2.5 [&>svg]:h-2.5 [&>svg]:stroke-[4] stroke-gray-dark hover:translate-y-0.5"
   >
     <slot />
   </div>
